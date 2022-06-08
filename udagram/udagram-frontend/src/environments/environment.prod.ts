04ba34beed874fafcbe4f2126b2c1b873c3268af
Amro-Khairi <wd.amrokhairi@gmail.com>
2022-06-08 21:46:18 +0200

    modifying scripts && configuring circleCi

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://udacity-aws-project.eba-ekhwgb8f.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://udacity-aws-project.eba-ekhwgb8f.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
